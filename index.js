7fc098dba909d2234a69ee86dc2cb58fc7f7f5aa
Aluisius95 <120209413+Aluisius95@users.noreply.github.com>
2023-05-09 18:34:35 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,21 @@
+// Import stylesheets
+import './style.css';
+
+// Write Javascript code!
+const appDiv = document.getElementById('app');
+appDiv.innerHTML = `<h1>JS Starter</h1>`;
+
+const tryThis = {
+  name:"ciao",
+  user:"ciaone",
+  star:function()
+    {return 'Stampa nome: ' + this.user }
+};
+
+class users {
+  constructor(firstName, lastName);
+}
+
+var first = 
+
+appDiv.innerHTML = users;
