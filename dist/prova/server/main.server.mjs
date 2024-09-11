5d63a78ffc215f7e3937e822d9dcabe549172788
Luigi <120209413+Aluisius95@users.noreply.github.com>
2024-09-11 17:35:59 +0200

    build modifica

--- dist/prova/server/main.server.mjs
+++ dist/prova/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-H3RKY3XA.mjs";import"./chunk-TEKMSGO5.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
+import{a}from"./chunk-YDI3N274.mjs";import"./chunk-DR2KFOM2.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
